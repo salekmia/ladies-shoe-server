f53ca93e9d6c5714462ef6b3107ac03dc96e8312
Abu Salek <bhairabbari330@gmail.com>
2021-11-11 23:50:32 +0600

    method added for my order, manage order and purchase

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const { MongoClient } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config();
- 
+const ObjectId = require('mongodb').ObjectId;
 
 const port = process.env.PORT || 5000;
  
@@ -18,14 +18,123 @@ async function run() {
     try {
       await client.connect();
       const database = client.db("ladies_shoe");
-      const usersCollection = database.collection("users"); 
+      const usersCollection = database.collection("users");
+      const productsCollection = database.collection("products") 
+      const reviewsCollection = database.collection("reviews");
+      const ordersCollection = database.collection("orders")
      
-    // post method for users
+        // post method for users
         app.post('/users', async(req, res) => {
             const user = req.body;
             const result = await usersCollection.insertOne(user)
             res.json(result)
             console.log(result)
+        })
+
+        // post method for products
+        app.post('/products', async(req, res) => {
+            const product = req.body;
+            const result = await productsCollection.insertOne(product);
+            res.json(result)
+            console.log(result)
+        })
+
+        // porst method for reviews
+        app.post('/reviews', async(req, res) => {
+            const review = req.body;
+            const result = await reviewsCollection.insertOne(review)
+            res.json(result)
+            console.log(result)
+        })
+
+        // post method for orders
+        app.post('/orders', async(req, res) => {
+            const order = req.body;
+            const result = await ordersCollection.insertOne(order)
+            res.json(result)
+            console.log(result)
+        })
+
+        // get method for products
+        app.get('/products', async(req, res) => {
+            const products = productsCollection.find({})
+            const result = await products.toArray()
+            res.json(result)
+        })
+
+        // get method for orders
+        app.get('/orders', async(req, res) => {
+            const orders = ordersCollection.find({})
+            const result = await orders.toArray()
+            res.json(result)
+        })
+
+        // delete method for order delete
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await ordersCollection.deleteOne(query);
+            res.json(result);
+        })
+
+
+        // get special product by id
+        app.get('/products/:id', async(req, res) => {
+            const id = req.params.id;
+            console.log('getting specific product', id);
+            const query = { _id: ObjectId(id) };
+            const product = await productsCollection.findOne(query)
+            res.json(product)
+        })
+
+        // put method for users
+        app.put('/users', async(req, res) => {
+            const user = req.body;
+            const filter = {email: user.email}
+            const options = {upsert: true}
+            const updateDoc = {$set: user}
+            const result = await usersCollection.updateOne(filter, updateDoc, options)
+            res.json(result)
+        })
+
+
+        app.put('/users/admin', async(req, res) => {
+            const user = req.body;
+            console.log(user)
+            const filter = {email: user.email}
+            const updateDoc = {$set: {role: 'admin'}}
+            const result = await usersCollection.updateOne(filter, updateDoc)
+            res.json(result)
+            console.log(result)
+        })
+
+        app.get('/users/:email', async(req, res) => {
+            const email = req.params.email;
+            const query = {email: email}
+            const user = await usersCollection.findOne(query)
+            let isAdmin = false
+            if(user?.role === 'admin') {
+                isAdmin = true
+            }
+            res.json({admin: isAdmin})
+        })
+
+
+        // put method for updating status of order
+        app.put('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedOrders = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    status: updatedOrders.status
+                },
+            };
+            const booking = await ordersCollection.updateOne(filter, updateDoc, options);
+            console.log('updated api booking pending', id)
+            res.json(booking);
+
         })
         
 
